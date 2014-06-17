2f8e0663639dc042597adf1123954c871dd39a4a
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-06-17 14:05:54 +0300

    Preserving html in descriptions build

---
+++ dist/scripts/d515b4ca.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("iGemPlates2014App",["ui.utils"]),angular.module("iGemPlates2014App").controller("TeamCtrl",["$scope","$http",function(a,b){b.get("teams.json").success(function(b){console.log(b),a.entries=b})}]),angular.module("iGemPlates2014App").filter("unsafe",["$sce",function(a){return function(b){return a.trustAsHtml(b)}}]);
