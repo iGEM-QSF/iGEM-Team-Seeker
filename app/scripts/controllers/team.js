601bc9ced056700bc3648a5f4ccc0f3ba5f0d219
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-06-17 14:04:30 +0300

    preserving html in team descriptions

--- app/scripts/controllers/team.js
+++ app/scripts/controllers/team.js
@@ -3,7 +3,14 @@
 angular.module('iGemPlates2014App')
   .controller('TeamCtrl', function ($scope, $http) {
     $http.get('teams.json').success( function(data, status, headers, config) {
-        console.log(data)
+        console.log(data);
         $scope.entries = data;
     });
   });
+
+angular.module('iGemPlates2014App').filter('unsafe', function($sce) {
+    return function(val) {
+        return $sce.trustAsHtml(val);
+    };
+});
+
