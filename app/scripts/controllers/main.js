7c1428395d10af49a0f1cea07faba784a6912572
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-06-17 13:29:01 +0300

    removing BBSeeker things

--- app/scripts/controllers/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('iGemPlates2014App')
-  .controller('MainCtrl', function ($scope, $http) {
-    $http.get('allkitplates.json').success( function(data, status, headers, config) {
-        console.log(data)
-        $scope.entries = data;
-    });
-  });
