c541c5b56c74c1c96de6a592fa6428e8d50b709d
Lassi Vapaakallio <lassi.vapaakallio@cs.helsinki.fi>
2014-07-02 11:37:20 +0300

    Phrase search actually working, super slowly, but working

--- app/scripts/controllers/team.js
+++ app/scripts/controllers/team.js
@@ -1,47 +1,51 @@
 'use strict';
 
 angular.module('iGemPlates2014App')
-  .controller('TeamCtrl', function ($scope, $http) {
-    $http.get('teams.json').success( function(data, status, headers, config) {
-        console.log(data)
-        $scope.entries = data;
-    });
+.controller('TeamCtrl', function ($scope, $http) {
+	$http.get('teams.json').success( function(data, status, headers, config) {
+		//console.log(data)
+		$scope.entries = data;
+	});
 
-    $scope.criteria = {};
-    $scope.criteria.Abstract = "";
+	$scope.criteria = {};
+	$scope.criteria.Abstract = "";
 
-    $scope.phraseOrder = function(entry) {
-    	//console.log($scope.criteria.Abstract);
-   		return 1;
-	};
-
-    function tfidf(entry){
+	$scope.phraseSearch = function() {
 	// This function ranks the given documents according to query document
 	
-	// Preprocessing
-	for (var i = 0; i < documentList.length; i++) {
-		documentList[i] = process(documentList[i]);
+	if($scope.entries == null){
+		return $scope.entries;
 	}
-	queryWords = process(queryText);
+
+	// Preprocessing
+	for (var i = 0; i < $scope.entries.length; i++) {
+		$scope.entries[i].SearchText = process($scope.entries[i].Abstract);
+	}
+	var queryWords = process($scope.criteria.Abstract);
+	console.log(queryWords);
+	console.log($scope.entries.length);
 	// Calculating word count per document
 	var wordAndCount = []
 	for (var i = 0; i < queryWords.length; i++) {
 		var word = queryWords[i];
 		var countList = [];
-		for (var j = 0; j < documentList.length; j++){
+		for (var j = 0; j < $scope.entries.length; j++){
 			var count = 0;
-			var documentWords = documentList[j];
-			for (k = 0; k < documentWords.length; k++){
+			var documentWords = $scope.entries[j].SearchText;
+			for (var k = 0; k < documentWords.length; k++){
 				if (word == documentWords[k]){
 					count ++;
 				}
 			}
 			countList.push(count);
-		wordAndCount.push([word, countList]);
+			wordAndCount.push([word, countList]);
 		}
 	}
+	console.log(countList);
+	console.log(wordAndCount);
+
 	// calculating score per document
-	var scores = new Array(documentList.length);
+	var scores = new Array($scope.entries.length);
 	for (var i = 0; i < wordAndCount.length; i++){
 		
 		// Calculating document count for each word
@@ -56,26 +60,25 @@ angular.module('iGemPlates2014App')
 			if (!scores[j]){
 				scores[j] = 0
 			}
-			scores[j] +=  Math.log(wordAndCount[i][1][j] + 1) * Math.log(documentList.length/(1+ documentCount)) // tf * idf
+			scores[j] +=  Math.log(wordAndCount[i][1][j] + 1) * Math.log($scope.entries.length/(1+ documentCount)) // tf * idf
 		}
 	}
 	
 	// Producing the final result and sorting it
 	var resultsList = []
-	for (var i = 0; i < documentList.length; i++){
-		resultsList.push([i, scores[i]]);
+	for (var i = 0; i < $scope.entries.length; i++){
+		$scope.entries[i].Score = scores[i];
 	}
-	resultsList.sort(function(a,b){
-		return b[1]- a[1];
-	});
-	return resultsList;
+
+
+	return $scope.entries;
 }
 
 function process(text) {
 	/*
 	 * Delete all punctuation characters, short words and stop words in query text and documents
 	 */
-	
+
 	// replace capital letters and remove punctuation
 	text = text.toLowerCase();
 	text = text.replace(/[\.,!?]/g,"");
@@ -84,12 +87,12 @@ function process(text) {
 	var pattern = / .{1,3} /g;
 	text = text.replace(pattern, " ");
 	
-	splitted = text.split(" ");
+	var splitted = text.split(" ");
 	
 	
 	// Stop words are common words that should be removed before similarity test
-	var stopWords = ["a","and", "the", "that", "so","which", "but", "are",
-	                 "is", "also", "though","it", "that",];
+	var stopWords = ["that","but", "we", "the", "which","a", "as", "an",
+	"then", "also", "just","this", "vaan","is"];
 	for (var i = 0; i < stopWords.length; i++){
 		var word = stopWords[i];
 		while (splitted.indexOf(word) !== -1){
@@ -98,4 +101,8 @@ function process(text) {
 	}
 	return splitted;
 }
-  });
+
+$scope.getScore = function(entry){
+	return entry.Score *(-1);
+}
+});
